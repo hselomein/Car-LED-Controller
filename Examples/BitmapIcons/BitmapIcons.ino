abb385e7541bc30af002898353c66be3234102c6
Yves Avady <yavady@gmail.com>
2023-05-05 20:14:58 -0400

    test

--- Examples/BitmapIcons/BitmapIcons.ino
+++ Examples/BitmapIcons/BitmapIcons.ino
@@ -205,7 +205,7 @@ void loop() {
 
   for (int xPos = 0; xPos < PANEL_RES_X; xPos++ ) {
     for (int yPos = 0; yPos < PANEL_RES_Y; yPos++ ) {
-      uint16_t color = xPos % 4 + yPos * 16;
+      uint16_t color = xPos / 4;
       dma_display->drawPixel(xPos, yPos, color);
     }
   }
