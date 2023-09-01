c1c49db2715a6de29ad9daadc8413a032abc6021
Corey Davis <hselomein@gmail.com>
2023-09-01 09:27:08 -0400

    added alternate pinouts for lcd matrix

--- src/main.cpp
+++ src/main.cpp
@@ -87,6 +87,7 @@ static float curHorn   = 0.0f;
 
   //LED Martrix pin section
   //This is configured using a P2 64x64 LED Matrix, which has an E pin.
+  //Pinout for LED Matrix Controller V8
   #define R1_PIN  32
   #define G1_PIN  33
   #define B1_PIN  25
@@ -101,6 +102,56 @@ static float curHorn   = 0.0f;
   #define LAT_PIN 17
   #define OE_PIN  5
   #define CLK_PIN 16
+/*  
+Use this alertnate pinout for LED Matrix Controller V9 or Yves Version
+PCB V8
+  #define R1_PIN  25
+  #define G1_PIN  26
+  #define B1_PIN  33
+  #define R2_PIN  14
+  #define G2_PIN  12
+  #define B2_PIN  13
+  #define A_PIN   27
+  #define B_PIN   2 
+  #define C_PIN   5
+  #define D_PIN   17
+  #define E_PIN   32  
+  #define LAT_PIN 4
+  #define OE_PIN  15
+  #define CLK_PIN 16
+  
+working pinout 2
+  #define R1_PIN  32
+  #define G1_PIN  33
+  #define B1_PIN  25
+  #define R2_PIN  26
+  #define G2_PIN  27
+  #define B2_PIN  14
+  #define A_PIN   13
+  #define B_PIN   15 
+  #define C_PIN   2
+  #define D_PIN   4
+  #define E_PIN   12  
+  #define LAT_PIN 17
+  #define OE_PIN  5
+  #define CLK_PIN 16
+  
+wave share pinout
+  #define R1_PIN  25
+  #define G1_PIN  26
+  #define B1_PIN  27
+  #define R2_PIN  14
+  #define G2_PIN  12
+  #define B2_PIN  13
+  #define A_PIN   23
+  #define B_PIN   22 
+  #define C_PIN   5
+  #define D_PIN   17
+  #define E_PIN   32  
+  #define LAT_PIN 4
+  #define OE_PIN  15
+  #define CLK_PIN 16
+  */
 
   //LED Matrix Initialization
   #define PANEL_RES_X 64  // Number of pixels wide of each INDIVIDUAL panel module.
