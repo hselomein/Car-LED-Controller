dcb2cf9b56a35b50432f8a780427b1e9a9215161
Yves Avady <yavady@gmail.com>
2023-10-27 16:32:58 -0400

    remove code from header files

--- include/TaskLCD.h
+++ include/TaskLCD.h
@@ -10,37 +10,8 @@
 #define LCD_UPDATE_INTERVAL 150   // How fast to update LCD in ms
 hd44780_I2Cexp lcd;               // Declare lcd object: auto locate & config exapander chip
 
-void taskLCDUpdates( void * pvParameters ){
-  char tmpMessage[16];
-  lcd.clear();    //clear the display and home the cursor
-  sprintf(tmpMessage, "MODE DRL   Horn "); 
-  lcd.setCursor(0,0); //move cursor to 1st line on display
-  lcd.print(tmpMessage);
-  delay(50); 
+void taskLCDUpdates( void * pvParameters );
 
-  while(true){
-    if (curHorn > VOLT_BUF) {
-      sprintf(tmpMessage, "HORN %04.1fV %04.1fV", curDRL, curHorn); 
-    } else {
-      sprintf(tmpMessage, "%s %04.1fV %04.1fV", curMode.txtColor, curDRL, curHorn);  
-    }
-    lcd.setCursor(0,1); //move cursor to 2nd line on display
-    lcd.print(tmpMessage);
-    delay(LCD_UPDATE_INTERVAL);
-  }
-}
-
-void initTaskLCD() {
-    // Create task on Core 1 to Update LCD
-  xTaskCreatePinnedToCore(
-    taskLCDUpdates,   /* Function to implement the task */
-    "taskLCDUpdates", /* Name of the task */
-    10000,            /* Stack size in words */
-    NULL,             /* Task input parameter */
-    0,                /* Priority of the task */
-    NULL,             /* Task handle. */
-    1);               /* Core where the task should run */
-
-}
+void initTaskLCD();
 
 #endif // _TASKLCD_H_
