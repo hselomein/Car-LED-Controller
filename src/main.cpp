776541a8b535f20c5ff52eb4110d67351c9eeec0
Corey Davis <hselomein@gmail.com>
2024-03-07 16:13:11 -0500

    removed BTN_INTERRUPTS related code - working

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,6 @@
   #define LEFT_IND false    //enable left indicator code for testing
   #define RIGHT_IND false   //enable right indicator code for testing
   #define PCB_V9 true       //enble if you are using V9 LED Controller PCB
-  #define BTN_INTERRUPTS  false  //enable init buttons as interrupts - causes crashing
 
 //Arduino Standard
   #include <driver/adc.h>
@@ -505,17 +504,9 @@ void setup()
 #if PCB_V9 
   Horn_Button = new Button(HORN_PIN, true); //this is for an inverted setup where HIGH is the idle state of the buttons
   Mode_Button = new ButtonPullup(MODE_PIN);
-#if BTN_INTERRUPTS == false 
   Ind_L_Button = new Button(IND_L_PIN, true);
   Ind_R_Button = new Button(IND_R_PIN, true);
 #endif
-#endif
-
-#if BTN_INTERRUPTS  
-  //simplebutton inturrupt setup
-  attachInterrupt(IND_L_PIN, left_indicator, FALLING);
-  attachInterrupt(IND_R_PIN, right_indicator, FALLING);
-#endif  
 
   // Configure ADC
   if (DEBUG) {Serial.println("Start Init ADC");}
@@ -603,10 +594,8 @@ void loop()
 #if PCB_V9 
   Horn_Button->update();
   //Mode_Button->update();
-#if BTN_INTERRUPTS == false   
   Ind_L_Button->update();
   Ind_R_Button->update();
-#endif
 #endif
 
   if (firstLoop) {
@@ -634,17 +623,13 @@ void loop()
 #endif
 #if PCB_V9 
   bool currentHornButtonState = Horn_Button->getState();
-#if BTN_INTERRUPTS == false   
   bool currentInd_LButtonState = Ind_L_Button->getState();
   bool currentInd_RButtonState = Ind_R_Button->getState();
-#endif
 
     if (!currentHornButtonState) {
       leds.fill(ANGRY_COLOR);
       hornState = "BEEP";
     }
-   
-#if BTN_INTERRUPTS == false    
     else if (!currentInd_RButtonState && !currentInd_LButtonState) { 
       indStatus = HAZARD;
       }
@@ -654,7 +639,6 @@ void loop()
     else if (!currentInd_LButtonState && currentInd_RButtonState) { 
       indStatus = LEFT; 
       }
-#endif    
     else {
     leds.fill(curMode.curColor);
     hornState = "OFF ";
