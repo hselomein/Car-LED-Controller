bd4c2dbd2e2b481a073db2ef411daed42c73c8f0
Yves Avady <yavady@gmail.com>
2024-10-07 18:21:42 -0400

    Change to working with a string that is a binary representation

--- Docs/2022 Ford Escape Stuff/Scripts/parseLinDatafromFile.py
+++ Docs/2022 Ford Escape Stuff/Scripts/parseLinDatafromFile.py
@@ -1,72 +1,85 @@
 def parse_lin_frame(data):
+    #parse_lin_frame1(data)
+    #return
+
     # Assuming data is a bytearray containing the LIN frame
     index = 0
-    dataCpy = int.from_bytes(data, byteorder='big') # create a copy of data to prevent altering the original
-    #print(f"Data: {dataCpy}")
+    binaryString = ''.join(format(byte, '08b') for byte in data) # Get a binary string of the data
+    print(f"Binary Data: {binaryString}")
     dataLength = len(data) -1 # prevent calculating length every time
     frames = []
 
-    while dataCpy.bit_length() > 32: #skip last 4 bytes
+    while len(binaryString) > 32: #skip last 4 bytes
+        binaryString = binaryString[-(len(binaryString)-1):] # Shift data by 1 Bit
+        #print(f"Binary Data: {binaryString}")
+        
         # Sync Break: At least 13 dominant bits (0x00)
-        msb13 = getMSB(dataCpy, 13)
-        print(f"13 MSB: {msb13}")
-        #print(f"Data: {dataCpy}")
-        if getMSB(dataCpy, 13) == 0:
-            print(f"Data: {dataCpy}")
-            sync_break = dataCpy[index]
-            dataCpy = removeMSB(dataCpy) # Shift data by 1 Bit
+        if binaryString[:13] == '0000000000000':
+            syncBreak = ''
+            while binaryString[:1] == '0':
+                binaryString = binaryString[-(len(binaryString)-1):] # Shift data by 1 Bit
+                syncBreak += '0'
+            syncBreakLength = len(syncBreak)
+            print(f"Break: {syncBreak}     Length: {syncBreakLength}")
 
             # Sync Field: 0x55
-            if index < dataLength and dataCpy[index] == 0x55:
-                sync_field = dataCpy[index]
-                index += 1
+            syncField = binaryString[:7] # get only seven bits as the leading 0 has been already removed
+            print(f"Sync: {syncField}")
+            if syncField == '1010101':
+                binaryString = binaryString[7:] # remove syncfield from data
 
                 # Identifier: 1 byte
-                if index < dataLength:
-                    identifier = dataCpy[index]
+                identifier = binaryString[:8]
+                print(f"ID: {identifier}")
+                binaryString = binaryString[8:]
+
+                continue
+
+                # Data Bytes: 0 to 8 bytes
+                data_bytes = []
+                sum_bytes = 0
+                while index < dataLength and len(data_bytes) < 8:
+                    data_bytes.append(data[index])
+                    sum_bytes += dataCpy[index]
                     index += 1
 
-                    # Data Bytes: 0 to 8 bytes
-                    data_bytes = []
-                    sum_bytes = 0
-                    while index < dataLength and len(data_bytes) < 8:
-                        data_bytes.append(data[index])
-                        sum_bytes += dataCpy[index]
-                        index += 1
+                sum_bytes &= 0xFF
+                sum_bytes = (~sum_bytes) & 0xFF
 
-                    sum_bytes &= 0xFF
-                    sum_bytes = (~sum_bytes) & 0xFF
+                # Checksum: 1 byte
+                if index < dataLength:
+                    checksum = dataCpy[index]
+                    index += 1
 
-                    # Checksum: 1 byte
-                    if index < dataLength:
-                        checksum = dataCpy[index]
-                        index += 1
+                    # check checksum
+                    csmatch = (sum_bytes == checksum)
 
-                        # check checksum
-                        csmatch = (sum_bytes == checksum)
-
-                        # Store the parsed frame
-                        frame = {
-                            'sync_break': sync_break,
-                            'sync_field': sync_field,
-                            'identifier': identifier,
-                            'data_bytes': data_bytes,
-                            'checksum': checksum,
-                            'cs_match': csmatch
-                        }
-                        frames.append(frame)
-        else:
-            dataCpy = removeMSB(dataCpy) # Shift data by 1 Bit
-
+                    # Store the parsed frame
+                    frame = {
+                        'sync_break': syncBreak,
+                        'sync_field': syncField,
+                        'identifier': identifier,
+                        'data_bytes': data_bytes,
+                        'checksum': checksum,
+                        'cs_match': csmatch
+                    }
+                    frames.append(frame)
     return frames
 
-def getMSB(val, numBits):   # Get most significant bits
+def getBits(val, numBits=1, startBit=0):   # Get most significant bits
     #intVal = int.from_bytes(val[0:numBits//8+1], byteorder='big')
-    return val >> (val.bit_length() - numBits)
+    if startBit > 0 :
+        newVal = removeMSB(val, startBit)
+    else :
+        newVal = val
+    return newVal >> (newVal.bit_length() - numBits)
 
-def removeMSB(val): # remove most significant bit using mask and bitwise and operator
-    mask = ~(1 << (val.bit_length() - 1))  # create a mask with all bits set except MSB
-    return val & mask
+def removeMSB(val, numBits=1): # remove most significant bits using mask and bitwise and operator
+    newVal = val
+    for i in range(numBits):
+        mask = ~(1 << (newVal.bit_length() - 1))  # create a mask with all bits set except MSB
+        newVal &=mask
+    return newVal
 
 def verify_checksum(frame, enhanced=True):
     if enhanced:
@@ -87,62 +100,62 @@ def identify_frame_type(frame):
 
 import argparse
 
-#def parse_lin_frame(data):
-#    # Assuming data is a bytearray containing the LIN frame
-#    index = 0
-#    frames = []
-#
-#    while index < len(data):
-#        # Sync Break: At least 13 dominant bits (0x00)
-#        if data[index] == 0x00:
-#            sync_break = data[index]
-#            index += 1
-#            while index < len(data) and data[index] == 0x00:
-#                index += 1
-#
-#            # Sync Field: 0x55
-#            if index < len(data) and data[index] == 0x55:
-#                sync_field = data[index]
-#                index += 1
-#
-#                # Identifier: 1 byte
-#                if index < len(data):
-#                    identifier = data[index]
-#                    index += 1
-#
-#                    # Data Bytes: 0 to 8 bytes
-#                    data_bytes = []
-#                    sum_bytes = 0
-#                    while index < len(data) and len(data_bytes) < 8:
-#                        data_bytes.append(data[index])
-#                        sum_bytes += data[index]
-#                        index += 1
-#
-#                    sum_bytes &= 0xFF
-#                    sum_bytes = (~sum_bytes) & 0xFF
-#
-#                    # Checksum: 1 byte
-#                    if index < len(data):
-#                        checksum = data[index]
-#                        index += 1
-#
-#                        # check checksum
-#                        csmatch = (sum_bytes == checksum)
-#
-#                        # Store the parsed frame
-#                        frame = {
-#                            'sync_break': sync_break,
-#                            'sync_field': sync_field,
-#                            'identifier': identifier,
-#                            'data_bytes': data_bytes,
-#                            'checksum': checksum,
-#                            'cs_match': csmatch
-#                        }
-#                        frames.append(frame)
-#        else:
-#            index += 1
-#
-#    return frames
+def parse_lin_frame1(data):
+    # Assuming data is a bytearray containing the LIN frame
+    index = 0
+    frames = []
+
+    while index < len(data):
+        # Sync Break: At least 13 dominant bits (0x00)
+        if data[index] == 0x00:
+            sync_break = data[index]
+            index += 1
+            while index < len(data) and data[index] == 0x00:
+                index += 1
+
+            # Sync Field: 0x55
+            if index < len(data) and data[index] == 0x55:
+                sync_field = data[index]
+                index += 1
+
+                # Identifier: 1 byte
+                if index < len(data):
+                    identifier = data[index]
+                    index += 1
+
+                    # Data Bytes: 0 to 8 bytes
+                    data_bytes = []
+                    sum_bytes = 0
+                    while index < len(data) and len(data_bytes) < 8:
+                        data_bytes.append(data[index])
+                        sum_bytes += data[index]
+                        index += 1
+
+                    sum_bytes &= 0xFF
+                    sum_bytes = (~sum_bytes) & 0xFF
+
+                    # Checksum: 1 byte
+                    if index < len(data):
+                        checksum = data[index]
+                        index += 1
+
+                        # check checksum
+                        csmatch = (sum_bytes == checksum)
+
+                        # Store the parsed frame
+                        frame = {
+                            'sync_break': sync_break,
+                            'sync_field': sync_field,
+                            'identifier': identifier,
+                            'data_bytes': data_bytes,
+                            'checksum': checksum,
+                            'cs_match': csmatch
+                        }
+                        frames.append(frame)
+        else:
+            index += 1
+
+    return frames
 
 def main():
     parser = argparse.ArgumentParser(description="Parse LIN bus data from a binary file.")
